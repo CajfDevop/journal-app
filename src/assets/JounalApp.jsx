761bd654d8a778918c473cfd3bf233033e557037
jaracar84 <cajfdeveloper@gmail.com>
2024-07-04 15:45:35 -0500

    proyecto empezado | journal app

---
+++ src/assets/JounalApp.jsx
@@ -0,0 +1,7 @@
+export const JounalApp = () => {
+  return (
+    <>
+      <h1>Hola Mundo</h1>
+    </>
+  );
+};
---
+++ src/main.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+import './styles.css';
+import { JounalApp } from './assets/JounalApp.jsx';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <JounalApp />
+  </React.StrictMode>
+);
